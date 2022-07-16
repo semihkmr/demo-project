66c58d99e9f3b6776a9dfd6ce143b1908cca2a3a
semihkmr <komursemih1@gmail.com>
2022-07-16 10:27:42 -0400

    trigger practice

--- src/test/java/cydeo/Driver.java
+++ src/test/java/cydeo/Driver.java
@@ -34,6 +34,7 @@ public class Driver {
             switch (browser) {
                 case "remote-chrome":
                     try {
+
                         // assign your grid server address
                         String gridAddress = "52.90.101.317";
                         URL url = new URL("http://" + gridAddress + ":4444/wd/hub");
